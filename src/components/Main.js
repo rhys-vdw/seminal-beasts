04b25e7d25fb187fafc542d73ae41ffda040fb69
Rhys van der Waerden <rhys.vdw@gmail.com>
2016-08-29 22:50:30 +1000

    Use hash

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,56 @@
 import React, { PureComponent } from 'react'
 import generateCreature from '../Generation'
 import Creature from './Creature'
-import Random from 'random-js'
+import random from '../random'
 
-const mt = Random.engines.mt19937().autoSeed()
-
-const nextSeed = Random.integer(0, Math.pow(2, 31) - 1)
+const nextSeed = () => random.integer(0, Math.pow(2, 31))
+const getHash = () => window.location.hash.substr(1)
 
 export default class Main extends PureComponent {
   constructor(props) {
     super(props)
-    this.state = { creature: generateCreature() }
+    const hash = getHash()
+    debugger
+    const seed = hash.length === 0 ? nextSeed() : parseInt(hash)
+    this.state = { creature: generateCreature(seed), seed }
     this.handleClick = this.handleClick.bind(this)
+    this.handleHashChange = this.handleHashChange.bind(this)
+  }
+
+  componentDidMount() {
+    window.onhashchange = this.handleHashChange
+  }
+
+  componentWillUnmount() {
+    window.onhashchange = null
+  }
+
+  handleHashChange() {
+    this.setSeed(getHash())
+  }
+
+  setSeed(seed) {
+    window.location.hash = seed
+    this.setState({
+      creature: generateCreature(seed), seed
+    })
   }
 
   handleClick() {
-    const seed = nextSeed(mt)
-    this.setState({ creature: generateCreature(seed) })
+    this.setSeed(nextSeed())
   }
 
   render() {
-    const { creature } = this.state
+    const { creature, seed } = this.state
 
     return (
-      <div className='Main'
-        onClick={this.handleClick}
-      >
-        <Creature creature={creature} width={1024} height={768}/>
+      <div className='Main'>
+        <a href={`#${seed}`}>
+          Save
+        </a>
+        <div onClick={this.handleClick} >
+          <Creature creature={creature} width={1024} height={768}/>
+        </div>
       </div>
     );
   }
