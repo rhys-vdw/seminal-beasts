29d23be666ad1d5710d2f524370f530597fc3ad0
Rhys van der Waerden <rhys.vdw@gmail.com>
2016-09-04 12:47:24 +1000

    Select crossover method

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,63 @@ import random from '../random'
 import CopyToClipboard from 'react-copy-to-clipboard'
 import Color from 'tinycolor2'
 
-
 import { createGenome } from '../genome/GeneFactory'
 import { fromGenome } from '../genome/NodeFactory'
+import { mergeWith, isNumber, zipWith } from 'lodash'
+import { lerp } from '../Math'
+
+const INTERLEAVE_NODES = 'INTERLEAVE_NODES'
+const INTERLEAVE_GENES = 'INTERLEAVE_GENES'
+const SPLICE = 'SPLICE'
+const AVERAGE = 'AVERAGE'
+const RANDOM_LERP = 'RANDOM_LERP'
+
+function interleaveGenomesNodes(left, right) {
+  return zipWith(left, right, (l, r) => random.bool() ? l : r)
+}
+
+function spliceGenomes(left, right) {
+  const index = random.integer(0, left.length)
+  return random.bool()
+    ? left.slice(0, index).concat(right.slice(index))
+    : right.slice(0, index).concat(left.slice(index))
+}
+
+function interleaveGenomesGenes(left, right) {
+  return mergeWith([], left, right, (lValue, rValue) => {
+    if (isNumber(rValue)) {
+      return random.bool() ? lValue : rValue
+    }
+  })
+}
+
+function randomLerpGenomes(left, right) {
+  return mergeWith([], left, right, (lValue, rValue) => {
+    if (isNumber(rValue)) {
+      return lerp(lValue, rValue, random.real(0, 1, true))
+    }
+  })
+}
+
+function averageGenomes(left, right) {
+  return mergeWith([], left, right, (lValue, rValue) => {
+    if (isNumber(rValue)) {
+      return (rValue + lValue) / 2
+    }
+  })
+}
+
+function getCrossover(method) {
+  switch (method) {
+    case INTERLEAVE_NODES: return interleaveGenomesNodes
+    case INTERLEAVE_GENES: return interleaveGenomesGenes
+    case SPLICE: return spliceGenomes
+    case AVERAGE: return averageGenomes
+    case RANDOM_LERP: return randomLerpGenomes
+  }
+
+  throw new TypeError('Bad method: ' + method)
+}
 
 function generateCreature(seed) {
   const genome = createGenome(seed)
@@ -33,15 +87,38 @@ function updateWindow(seed, creature) {
   setTitle(creature)
 }
 
+function initialGenomes() {
+  const leftSeed = nextSeed()
+  const rightSeed = nextSeed()
+  const leftGenome = createGenome(leftSeed)
+  const rightGenome = createGenome(rightSeed)
+  const childGenome = interleaveGenomesNodes(leftGenome, rightGenome)
+  return { leftGenome, rightGenome, childGenome }
+  //console.log({ leftGenome, rightGenome, childGenome })
+  /*
+  return {
+    leftCreature: fromGenome(leftGenome),
+    rightCreature: fromGenome(rightGenome),
+    childCreature: fromGenome(childGenome),
+  }
+  */
+}
+
 export default class Main extends PureComponent {
   constructor(props) {
     super(props)
-    const hash = getHash()
-    const seed = hash.length === 0 ? nextSeed() : parseInt(hash)
-    const creature = generateCreature(seed)
-    updateWindow(seed, creature)
-    this.state = { creature, seed, hasBeenShared: false }
-    this.handleClick = this.handleClick.bind(this)
+    //const hash = getHash()
+    //const seed = isNaN(hash) ? nextSeed() : parseInt(hash)
+    //updateWindow(seed, creature)
+    this.state = {
+      ...initialGenomes(),
+      hasBeenShared: false,
+      crossover: interleaveGenomesNodes
+    }
+    this.handleCrossoverChange = this.handleCrossoverChange.bind(this)
+    this.handleChildClick = this.handleChildClick.bind(this)
+    this.handleRightParentClick = this.handleRightParentClick.bind(this)
+    this.handleLeftParentClick = this.handleLeftParentClick.bind(this)
     this.handleHashChange = this.handleHashChange.bind(this)
     this.handleCopy = this.handleCopy.bind(this)
   }
@@ -54,8 +131,20 @@ export default class Main extends PureComponent {
     window.onhashchange = null
   }
 
+  handleCrossoverChange(event) {
+    this.setState({ crossover: getCrossover(event.target.value) })
+    this.generateChildGenome()
+  }
+
   handleHashChange() {
     this.setSeed(getHash())
+  }
+
+  generateChildGenome() {
+    this.setState(prevState => {
+      const childGenome = prevState.crossover(prevState.leftGenome, prevState.rightGenome)
+      return { childGenome }
+    })
   }
 
   setSeed(seed) {
@@ -70,8 +159,18 @@ export default class Main extends PureComponent {
     })
   }
 
-  handleClick() {
-    this.setSeed(nextSeed())
+  handleLeftParentClick() {
+    this.setState({ leftGenome: createGenome(nextSeed()) })
+    this.generateChildGenome()
+  }
+
+  handleRightParentClick() {
+    this.setState({ rightGenome: createGenome(nextSeed()) })
+    this.generateChildGenome()
+  }
+
+  handleChildClick() {
+    this.generateChildGenome()
   }
 
   handleCopy() {
@@ -79,29 +178,66 @@ export default class Main extends PureComponent {
   }
 
   render() {
-    const { hasBeenShared, creature, seed } = this.state
-    const linkColor = maxLightness(creature.root.color, 0.9)
+    const {
+      hasBeenShared, childCreature, leftGenome, rightGenome, childGenome
+    } = this.state
+    //const linkColor = maxLightness(creature.root.color, 0.9)
     return (
       <div className='Main'>
-        <CopyToClipboard
-          text={window.location.toString()}
-          onCopy={this.handleCopy}
-        >
-          <a
-            className='Main-saveLink'
-            href={`#${seed}`}
-            style={{ color: linkColor.toRgbString() }}
+        {/*
+          <CopyToClipboard
+            text={window.location.toString()}
+            onCopy={this.handleCopy}
           >
-            { hasBeenShared
-                ? <span className='Main-copied'>♡ copied to clipboard ♡</span>
-                : <span className='Main-share'>share</span> }
-          </a>
-        </CopyToClipboard>
-        <div
-          className='Main-creatureContainer'
-          onClick={this.handleClick}
-        >
-          <Creature creature={creature} width='100%' height='100%'/>
+            <a
+              className='Main-saveLink'
+              href={`#${seed}`}
+              style={{ color: linkColor.toRgbString() }}
+            >
+              { hasBeenShared
+                  ? <span className='Main-copied'>♡ copied to clipboard ♡</span>
+                  : <span className='Main-share'>share</span> }
+            </a>
+          </CopyToClipboard>
+        */}
+        <div className='Main-options'>
+          <input id='interleaveNodes' name='crossover' type='radio' value={INTERLEAVE_NODES} defaultChecked onChange={this.handleCrossoverChange} />
+          <label htmlFor='interleaveNodes'>Interleave nodes</label>
+          <input id='interleaveGenes' name='crossover' type='radio' value={INTERLEAVE_GENES} onChange={this.handleCrossoverChange} />
+          <label htmlFor='interleaveGenes'>Interleave genes</label>
+          <input id='splice' name='crossover' type='radio' value={SPLICE} onChange={this.handleCrossoverChange} />
+          <label htmlFor='splice'>Splice</label>
+          <input id='average' name='crossover' type='radio' value={AVERAGE} onChange={this.handleCrossoverChange} />
+          <label htmlFor='average'>Average</label>
+          <input id='randomLerp' name='crossover' type='radio' value={RANDOM_LERP} onChange={this.handleCrossoverChange} />
+          <label htmlFor='randomLerp'>Random lerp</label>
+        </div>
+        <div className='Main-creatures'>
+          <div className='Main-parents'>
+            <div
+              className='Main-parentContainer'
+              onClick={this.handleLeftParentClick}
+            >
+              <label>Parent A</label>
+              <Creature creature={fromGenome(leftGenome)} width='100%' height='100%'/>
+            </div>
+            <div
+              className='Main-parentContainer'
+              onClick={this.handleRightParentClick}
+            >
+              <label>Parent B</label>
+              <Creature creature={fromGenome(rightGenome)} width='100%' height='100%'/>
+            </div>
+          </div>
+          <div className='Main-child'>
+            <div
+              className='Main-childContainer'
+              onClick={this.handleChildClick}
+            >
+              <label>Child of A and B</label>
+              <Creature creature={fromGenome(childGenome)} width='100%' height='100%'/>
+            </div>
+          </div>
         </div>
       </div>
     );
